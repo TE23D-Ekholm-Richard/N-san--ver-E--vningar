2d9b9730740bd2e2e1eba380ae751e99efc253b3
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2024-11-11 08:43:38 +0100

    bjbji

---
+++ Näsan över E Övningar/Program.cs
@@ -0,0 +1,35 @@
+﻿// if(6>=3){
+// Console.WriteLine("Hello world");
+
+// }
+
+
+
+// while(true){
+//     Console.WriteLine("Skriv användarnamn");
+
+// string name= Console.ReadLine();
+
+// Console.WriteLine("Skriv lösenord");
+// string lösenord = Console.ReadLine();
+
+
+// if(name=="kalleanka" && lösenord=="12345"){
+
+//     Console.WriteLine("Welcome");
+//     break;
+// }
+
+
+
+
+// else
+// {
+//     Console.WriteLine("Wrong username or password");
+// }
+
+
+// Console.ReadLine();
+// }
+// Console.ReadLine();
+
